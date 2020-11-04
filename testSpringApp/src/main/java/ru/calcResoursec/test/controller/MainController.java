085dfb237fa2e800d5bdf056f0b15e981fef6519
Yaroslavih <55427690+worfa@users.noreply.github.com>
2020-11-05 03:26:39 +0500

    add view check

--- testSpringApp/src/main/java/ru/calcResoursec/test/controller/MainController.java
+++ testSpringApp/src/main/java/ru/calcResoursec/test/controller/MainController.java
@@ -17,31 +17,21 @@ public class MainController {
 	@Autowired
 	private CheckRepository checkRepository;
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false,
-			defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
-	@GetMapping()
-	public String main(){
-		return "greeting";
-	}
-
 	@GetMapping("/add-Check")
 	public String getCheckForm(Map<String, Object> model) {
+		Iterable<Check> check = checkRepository.findAll();
+		model.put("check", check);
 		return "checkInp";
 	}
 
 	@PostMapping("/add-Check")
-	public String addNewCheck (@RequestParam String shopList
-			, @RequestParam Long sum, @RequestParam String date, Map<String, Object> model) {
-		Check check = new Check();
-		check.setShopList(shopList);
-		check.setSum(sum);
-		check.setDate(date);
+	public String addNewCheck (@RequestParam String shopList,
+			@RequestParam Long sum, @RequestParam String date, Map<String, Object> model) {
+		Check check = new Check(shopList, sum, date);
 		checkRepository.save(check);
-
+		
+		Iterable<Check> checkUp = checkRepository.findAll();
+		model.put("check", checkUp);
 		return "checkInp";
 	}
 
