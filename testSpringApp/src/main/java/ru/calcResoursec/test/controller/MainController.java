2031ce7c23297a29fd7af9af32a0781761051872
a070790 <56755728+a070790@users.noreply.github.com>
2020-11-24 19:37:01 +0500

    trying not to use a database write

--- testSpringApp/src/main/java/ru/calcResoursec/test/controller/MainController.java
+++ testSpringApp/src/main/java/ru/calcResoursec/test/controller/MainController.java
@@ -3,7 +3,6 @@ package ru.calcResoursec.test.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +10,8 @@ import ru.calcResoursec.test.model.Check;
 import ru.calcResoursec.test.model.Purchase;
 import ru.calcResoursec.test.model.User;
 import ru.calcResoursec.test.repository.CheckRepository;
-import ru.calcResoursec.test.repository.PurchaseRepository;
-import ru.calcResoursec.test.service.Appropriator;
 
 import java.util.Map;
-import java.util.regex.Pattern;
 
 @Controller
 public class MainController {
@@ -45,51 +41,46 @@ public class MainController {
 
 	@GetMapping("/add_check")
 	public String getCheckInputPage(Map<String, Object> model) {
-		model.put("chId", -1);
 		model.put("sum_", 0);
-		model.put("addPurchase", false);
-		model.put("wasSaved", false);
+		model.put("date_", "");
+
 		return "checkinput";
 	}
 
 	@PostMapping("/add_check")
 	public String addCheck(@AuthenticationPrincipal User user,
-						   @RequestParam Integer checkId,     @RequestParam String date,
-						   @RequestParam String name, 		  @RequestParam String category,
-						   @RequestParam Long price,  		  @RequestParam Integer quantity,
-						   @RequestParam Boolean addPurchase, @RequestParam Boolean wasSaved,
+						   @RequestParam String date,
+						   @RequestParam String purchaseName,
+						   @RequestParam String category,  		@RequestParam Long price,
+						   @RequestParam Integer quantity, @RequestParam Boolean isSaved,
 						   Map<String, Object> model) {
-		if (wasSaved) {
-			model.put("chId", checkId);
+		Check check = null;
+
+		if (date != "") {
+			if (check == null) {
+				long sum = 0;
+				check = new Check(user, date, sum);
+			}
+
+			check.addPurchase(new Purchase(purchaseName, category, price, quantity));
+
+			Long sum = check.getSum() + price * quantity;
+			check.setSum(sum);
+
+			model.put("sum_", sum);
+			model.put("date_", date);
+
+			return "checkinput";
+		}
+
+		if (date != "" && isSaved) {
+			checkRepository.save(check);
+
 			return "redirect:main";
 		}
 
-		if (addPurchase) {
-			Check check = checkRepository.findOneById(checkId);
-
-			if (check == null) {
-				check = new Check(user, date);
-				check.addPurchase(new Purchase(name, category, price, quantity));
-				Long sum = price * quantity;
-				check.setSum(sum);
-				model.put("sum_", sum);
-
-				checkRepository.save(check);
-
-				check = checkRepository.findOneByUserAndDate(user, date);
-				checkId = check.getId();
-				model.put("chId", checkId);
-			} else {
-				check.addPurchase(new Purchase(name, category, price, quantity));
-				Long sum = check.getSum() + price * quantity;
-				check.setSum(sum);
-				model.put("sum_", sum);
-
-				checkRepository.save(check);
-
-				model.put("chId", checkId);
-			}
-		}
+		model.put("sum_", 0);
+		model.put("date_", "");
 
 		return "checkinput";
 	}
